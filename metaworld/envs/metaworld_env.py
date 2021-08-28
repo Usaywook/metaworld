3f558c4268e680221c63ae22685d20ce8f3e94b1
usaywook <tpdnr1360@snu.ac.kr>
2021-08-29 00:09:50 +0900

    seed setting and render

--- metaworld/envs/metaworld_env.py
+++ metaworld/envs/metaworld_env.py
@@ -5,13 +5,15 @@ import metaworld
 
 class ML10Env(gym.Env):
     metadata = {'render.modes': ['human']}
-    def __init__(self):
+    def __init__(self, seed=None):
         super(ML10Env, self).__init__()
-        self.benchmark = metaworld.ML10()
-        self.set_train_env()
+        self.benchmark = metaworld.ML10(seed=seed)
+
         self.is_training = True
         self.goal_observable = True
+        self._scenario_idx = None
 
+        self.set_train_env()
         if self.goal_observable:
             self.env._partially_observable = False
 
@@ -38,7 +40,10 @@ class ML10Env(gym.Env):
     def set_train_env(self):
         envs = [(idx, name, env_cls) for idx, (name, env_cls) in enumerate(self.benchmark.train_classes.items())]
         self.num_envs = len(envs)
-        self.name_idx, self.name, env_cls = random.choice(envs)
+        if self._scenario_idx is not None:
+            self.name_idx, self.name, env_cls = envs[self._scenario_idx]
+        else:
+            self.name_idx, self.name, env_cls = random.choice(envs)
 
         tasks = [task for task in self.benchmark.train_tasks if task.env_name == self.name]
         self.num_tasks = len(tasks)
@@ -76,16 +81,20 @@ class ML10Env(gym.Env):
         self.is_training = True
         super().train()
 
+    def set_scenario(self, idx):
+        self._scenario_idx = idx
 
 class ML5Env(gym.Env):
     metadata = {'render.modes': ['human']}
-    def __init__(self):
+    def __init__(self, seed=None):
         super(ML5Env, self).__init__()
-        self.benchmark = metaworld.ML5()
-        self.set_train_env()
+        self.benchmark = metaworld.ML5(seed)
+
         self.is_training = True
         self.goal_observable = True
+        self._scenario_idx = None
 
+        self.set_train_env()
         if self.goal_observable:
             self.env._partially_observable = False
 
@@ -112,7 +121,10 @@ class ML5Env(gym.Env):
     def set_train_env(self):
         envs = [(idx, name, env_cls) for idx, (name, env_cls) in enumerate(self.benchmark.train_classes.items())]
         self.num_envs = len(envs)
-        self.name_idx, self.name, env_cls = random.choice(envs)
+        if self._scenario_idx is not None:
+            self.name_idx, self.name, env_cls = envs[self._scenario_idx]
+        else:
+            self.name_idx, self.name, env_cls = random.choice(envs)
 
         tasks = [task for task in self.benchmark.train_tasks if task.env_name == self.name]
         self.num_tasks = len(tasks)
@@ -128,7 +140,10 @@ class ML5Env(gym.Env):
         envs = [(idx, name, env_cls) for idx, (name, env_cls) in enumerate(self.benchmark.test_classes.items())]
         self.num_envs = len(envs)
 
-        self.name_idx, self.name, env_cls = random.choice(envs)
+        if self._scenario_idx is not None:
+            self.name_idx, self.name, env_cls = envs[self._scenario_idx]
+        else:
+            self.name_idx, self.name, env_cls = random.choice(envs)
 
         tasks = [task for task in self.benchmark.test_tasks if task.env_name == self.name]
         self.num_tasks = len(tasks)
@@ -151,15 +166,19 @@ class ML5Env(gym.Env):
         self.is_training = True
         super().train()
 
+    def set_scenario(self, idx):
+        self._scenario_idx = idx
 
 class MT10Env(gym.Env):
     metadata = {'render.modes': ['human']}
-    def __init__(self):
+    def __init__(self, seed=None):
         super(MT10Env, self).__init__()
-        self.benchmark = metaworld.MT10()
-        self.set_env()
-        self.goal_observable = True
+        self.benchmark = metaworld.MT10(seed=seed)
 
+        self.goal_observable = True
+        self._scenario_idx = None
+
+        self.set_env()
         if self.goal_observable:
             self.env._partially_observable = False
 
@@ -184,7 +203,10 @@ class MT10Env(gym.Env):
         envs = [(idx, name, env_cls) for idx, (name, env_cls) in enumerate(self.benchmark.train_classes.items())]
         self.num_envs = len(envs)
 
-        self.name_idx, self.name, env_cls = random.choice(envs)
+        if self._scenario_idx is not None:
+            self.name_idx, self.name, env_cls = envs[self._scenario_idx]
+        else:
+            self.name_idx, self.name, env_cls = random.choice(envs)
 
         tasks = [task for task in self.benchmark.train_tasks if task.env_name == self.name]
         self.num_tasks = len(tasks)
@@ -204,3 +226,6 @@ class MT10Env(gym.Env):
 
     def train(self):
         super().train()
+
+    def set_scenario(self, idx):
+        self._scenario_idx = idx
